dad677aa5f510406267c0f0e6e69ae620840112a
Guga Villar <grsv.21@gmail.com>
2023-09-01 18:31:21 -0300

    :heavy_plus_sign: Add auth0 env

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,6 +1,8 @@
 /// <reference types="vite/client" />
 interface ImportMetaEnv {
   readonly VITE_FAUNA_KEY: string
+  readonly VITE_AUTH0_CLIENT_ID: string
+  readonly VITE_AUTH0_DOMAIN: string
 }
 
 interface ImportMeta {
