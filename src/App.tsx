720a38936d08022696814212493426974c8f8da6
Guga Villar <grsv.21@gmail.com>
2023-09-01 18:38:31 -0300

    :sparkles: Ignore env file

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,35 @@
-import { Heading, Box } from '@chakra-ui/react'
+import { Fragment } from 'react'
+
+import { useAuth0 } from '@auth0/auth0-react'
+import { Heading, Box, Flex } from '@chakra-ui/react'
 
 import { ACUTAL_YEAR } from './common'
-import { ContainerCandidateListTable } from './common/components'
-import { ButtonDrawer } from './common/components/ButtonDrawer'
+import {
+  ContainerCandidateListTable,
+  ButtonDrawer,
+  ButtonAuthenticator
+} from './common/components'
 
 export const App = () => {
+  const { isAuthenticated } = useAuth0()
   return (
-    <Box p={4}>
-      <Heading textAlign='center'>
-        Apuração da eleição de conselheiro tutelar {ACUTAL_YEAR}
-      </Heading>
-      <ButtonDrawer />
-      <ContainerCandidateListTable />
-    </Box>
+    <Fragment>
+      <Flex
+        h={20}
+        width='full'
+        px={6}
+        align='center'
+        justify='space-evenly'
+      >
+        <Heading>
+          Apuração da eleição de conselheiro tutelar {ACUTAL_YEAR}
+        </Heading>
+        <ButtonAuthenticator />
+      </Flex>
+      <Box p={4}>
+        {isAuthenticated ? <ButtonDrawer /> : null}
+        <ContainerCandidateListTable />
+      </Box>
+    </Fragment>
   )
 }
